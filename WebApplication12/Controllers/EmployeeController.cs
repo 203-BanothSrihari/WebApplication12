ac074831a351c9925b2326c7e89b615bea6726cb
sriharibanoth <sreeharibanoth1203@gmail.com>
2022-01-15 17:24:15 +0530

    Add project files.

---
+++ WebApplication12/Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication12.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public string Index()
+        {
+            return "This is default action....";
+        }
+        public string FirstName()
+        {
+            
+            return "First Name is : Banoth.";
+            LastName();
+        }
+        public string LastName()
+        {
+            return "Last name is : Srihari";
+        }
+    }
+}
---
+++ WebApplication12/Controllers/HelloworldController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication12.Controllers
+{
+    public class HelloworldController : Controller
+    {
+        public string Index()
+        {
+            return "This is a default action";
+        }
+        public string Welcome()
+        {
+            return "This is a welcome action....";
+        }
+    }
+}
